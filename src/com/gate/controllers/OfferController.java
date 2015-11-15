1d0237b5aaa8cb9f86ce2e0724eee53ece6690ea
Neeraj Kumar <contactdhawan@gmail.com>
2015-11-15 14:38:30 -0500

    initial commit

---
+++ src/com/gate/controllers/OfferController.java
@@ -0,0 +1,25 @@
+package com.gate.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OfferController {
+	@RequestMapping("/")
+	
+	public ModelAndView displayHome(HttpSession session){
+		ModelAndView mav = new ModelAndView("home");
+		Map<String,Object> model= mav.getModel();
+		model.put("name", "sean");
+		session.setAttribute("name", "neeraj");
+		return mav;
+		//return "home";
+	}
+	
+}
